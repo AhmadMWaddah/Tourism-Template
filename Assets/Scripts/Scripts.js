479464a87c33ae8eadd82bb6098d6d6ce5501223
AhmadMWaddah <wddh.ahmd@gmail.com>
2024-06-30 19:58:23 +0300

    Testimonials Partition
    Created Testimonials Partition With Owl Carousel Also and It's Responsiveness and styles.

--- Assets/Scripts/Scripts.js
+++ Assets/Scripts/Scripts.js
@@ -59,4 +59,30 @@ window.addEventListener("load", () => {
 	});
 
 
+	/* =--------------------------------------= Testimonials Owl Carousel Slider =------------------= */
+	$("#testimonials-slider").owlCarousel({
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		margin: 10,
+		nav: true,
+		navText: [
+			"<i class='fa-solid fa-arrow-left'></i>",
+			"<i class='fa-solid fa-arrow-right'></i>"
+		],
+		responsive: {
+			0: {
+				items: 1,
+				nav: false
+			},
+			700: {
+				items: 2
+			},
+			1100: {
+				items: 3
+			},
+		}
+	});
+
+
 });
